85fa7e954cebb7b16d1cf7f1f37fb7935cac3564
zyx <zlukasbdf@hotmail.com>
2025-10-11 18:56:21 -0300

    Create auth-service types

--- packages/microservices/src/index.ts
+++ packages/microservices/src/index.ts
@@ -1,1 +1,3 @@
 export * from "./shared/index";
+export * from "./constants/index";
+export * from "./types/index";
--- packages/microservices/src/types/auth-service.types.ts
+++ packages/microservices/src/types/auth-service.types.ts
@@ -1,4 +1,56 @@
-
 export namespace AuthServiceTypes {
+  // Register
 
+  export interface RegisterUserInput {
+    email: string;
+    username: string;
+    password: string;
+  }
+
+  export interface RegisterUserOutput {
+    accessToken: string;
+    refreshToken: string;
+  }
+
+  // Login
+
+  export interface LoginUserInput {
+    email: string;
+    password: string;
+  }
+
+  export interface LoginUserOutput {
+    accessToken: string;
+    refreshToken: string;
+  }
+
+  // Refresh
+
+  export interface RefreshTokenInput {
+    refreshToken: string;
+  }
+
+  export interface RefreshTokenOutput {
+    accessToken: string;
+    refreshToken: string;
+  }
+
+  // Validate token
+
+  export interface ValidateTokenInput {
+    accessToken: string;
+  }
+
+  export type ValidateTokenOutput =
+    | {
+        isValid: true;
+        user: {
+          id: string;
+          username: string;
+          email: string;
+          updatedAt: Date;
+          createdAt: Date;
+        };
+      }
+    | { isValid: false; user: null };
 }
